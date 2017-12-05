5e6d7d8f972c8b72e54950c9a14e84de8923db52
Yusuke Sangenya <longinus.eva@gmail.com>
2017-12-06 03:52:33 +0900

    does not remove

--- lauch_db.sh
+++ lauch_db.sh
@@ -1,1 +1,1 @@
-docker run --rm -e POSTGRES_USER=aws -e POSTGRES_PASSWORD=aws -p 5432:5432 --name aws --volume pgsql-data:/var/lib/pgsql -d postgres:9.6-alpine
+docker run -e POSTGRES_USER=aws -e POSTGRES_PASSWORD=aws -p 5432:5432 --name aws --volume pgsql-data:/var/lib/pgsql/data -d postgres:9.6-alpine
